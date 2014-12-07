575c1762ca792b781ce47124c6d11501d527c1b1
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-07 14:45:30 -0500

    totally rewriting the send-message.sh script

--- send-message.sh
+++ send-message.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 workdir="/home/taylor/git/message-a-day"
-today=`date +%m/%d/%Y`
-message=`cat ${workdir}/messages |grep $today`
+today=`date +%m/%d`
+# sets $limit to the number of times the specificied string appears in the file
+limit=`grep -c "Today, I want to remind you" ${workdir}/messages`
+randomNum=$(($RANDOM % ${limit}))
+# check if today is a special day (e.g. anniversary, birthday, etc.)
+testCommand=`grep -c ${today} ${workdir}/messages`
 
-
+if [ $testCommand = 0 ] ; then
+  message=`grep ${today} ${workdir}/messages`
+else
+  message=`awk '/Today,\ I\ want\ to\ remind\ you/{i++}i==$(echo $limit)' ${workdir}/messages`
+fi
+echo $message
