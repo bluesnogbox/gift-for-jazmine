907264f7f98e6424cca39f34cc97f61f870668b7
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-07 22:16:08 -0500

    added support for pushbullet

--- send-message.sh
+++ send-message.sh
@@ -4,19 +4,24 @@ workdir="/home/taylor/git/message-a-day"
 today=`date +%m/%d`
 limit=$(wc -l < ${workdir}/messages)
 randomNum=$(($RANDOM % ${limit}))
+randomNum=`expr $randomNum + 1`
+pushbulletDir=
+pushbulletAPI=
+pushbulletDevice=
+
 # check if today is a special day (e.g. anniversary, birthday, etc.)
 testCommand=`grep -c ${today} ${workdir}/special-dates`
 
-# make sure the random number != 0
-if [ $randomNum = 0 ] ; then
-  while [ $randomNum = 0 ]; do
-    randomNum=$(($RANDOM % ${limit}))
-  done
-fi
+function send-message {
+  ${pushbulletDir}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice $message
+}
 
 if ! [ $testCommand = 0 ] ; then
   message=`grep ${today} ${workdir}/special-dates`
+  send-message
 else
-  message=`awk "NR==$randomNum { print; exit }" ${workdir}/messages`
+  if [ `date +%u` -eq 1 ] || [ `date +%u` -eq 3 ] || [ `date +%u` -eq 6 ]; then
+    message=`awk "NR==$randomNum { print; exit }" ${workdir}/messages`
+    send-message
+  fi
 fi
-echo $message
