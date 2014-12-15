7e2bd21879cd8ee0597c4a6cf08af5982d8d5849
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-14 19:46:30 -0500

    merged changes from testing

--- send-message.sh
+++ send-message.sh
@@ -5,12 +5,13 @@ today=`date +%m/%d`
 limit=$(wc -l < ${workdir}/messages)
 randomNum=$(($RANDOM % ${limit}))
 randomNum=`expr $randomNum + 1`
-pushbulletDir=P81dRv12Jp7cdza1zCbxyDidnyOl7YSV
-pushbulletAPI=ufenwEfsjAiVsKnSTs
+pushbulletDir="/home/taylor/bin/pyPushBullet"
+pushbulletAPI=lV3oFozSwiGAhbYYfgVFueq1A6Lm8PaP
 pushbulletDevice=
+randomDays=(`cat "${workdir}/randomdays"`)
 
 # check if today is a special day (e.g. anniversary, birthday, etc.)
-testCommand=`grep -c ${today} ${workdir}/special-dates`
+testCommand=`grep -c ${today} "${workdir}/special-dates"`
 
 function send-message {
   ${pushbulletDir}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice $message
@@ -20,8 +21,10 @@ if ! [ $testCommand = 0 ] ; then
   message=`grep ${today} ${workdir}/special-dates`
   send-message
 else
-  if [ `date +%u` -eq 1 ] || [ `date +%u` -eq 3 ] || [ `date +%u` -eq 6 ]; then
+today=`date +%u`
+if [[ $today -eq ${randomDays[0]} ]] || [[ $today -eq ${randomDays[1]} ]] || [[ $today -eq ${randomDays[2]} ]]; then
     message=`awk "NR==$randomNum { print; exit }" ${workdir}/messages`
-    send-message
+    #send-message
+    echo "$message"
   fi
 fi
