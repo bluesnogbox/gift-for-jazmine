51fb3fcc266b52109abfb017a7d7b85c481207b1
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-14 20:11:27 -0500

    removed pushbullet; instead, going with sms

--- send-message.sh
+++ send-message.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
 workdir="/home/taylor/git/message-a-day"
+
+#if [[ `date +%u` -eq 7 ]]; then
+#  echo "" > ${workdir}/randomdays
+
+#  days=()
+
+#  days+=($(( ( RANDOM % 7 ) + 1 )))
+#  days+=($(( ( RANDOM % 7 ) + 1 )))
+#  while [[ ${days[0]} -eq ${days[1]} ]]; do
+#    unset days[1]
+#    days+=($(( ( RANDOM % 7 ) + 1 )))
+#  done
+#  days+=($(( ( RANDOM % 7 ) + 1 )))
+#  while [[ ${days[2]} -eq ${days[1]} ]] || [[ ${days[2]} -eq ${days[0]} ]]; do
+#    unset days[2]
+#    days+=($(( ( RANDOM % 7 ) + 1 )))
+#  done
+#  echo ${days[@]} > ${workdir}/randomdays
+#fi
+
 today=`date +%m/%d`
 limit=$(wc -l < ${workdir}/messages)
 randomNum=$(($RANDOM % ${limit}))
 randomNum=`expr $randomNum + 1`
-pushbulletDir="/home/taylor/bin/pyPushBullet"
-pushbulletAPI=P81dRv12Jp7cdza1zCbxyDidnyOl7YSV
-pushbulletDevice=ufenwEfsjAiVsKnSTs
+phoneNum="8149773797"
+carrier="txt.mms.net"
 randomDays=(`cat "${workdir}/randomdays"`)
 
 # check if today is a special day (e.g. anniversary, birthday, etc.)
 testCommand=`grep -c ${today} "${workdir}/special-dates"`
 
 function send-message {
-  ${pushbulletDir}/pushbullet_cmd.py $pushbulletAPI note $pushbulletDevice $message
+  echo "$message" | mail -s "Hey you!" ${phoneNum}@${carrier}
 }
 
 if ! [ $testCommand = 0 ] ; then
@@ -24,7 +43,7 @@ else
 today=`date +%u`
 if [[ $today -eq ${randomDays[0]} ]] || [[ $today -eq ${randomDays[1]} ]] || [[ $today -eq ${randomDays[2]} ]]; then
     message=`awk "NR==$randomNum { print; exit }" ${workdir}/messages`
-    #send-message
+    send-message
     echo "$message"
   fi
 fi
