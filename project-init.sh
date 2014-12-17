7a748117c58d6bc24f076c538322fc5837e6161e
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-16 19:02:16 -0500

    added a script to init

---
+++ project-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+echo "I hope you like the gift I've been working on for you. You can read about it at https://github.com/bluesnogbox/gift-for-jazmine" | mail -s "Hey You!" 8149773797@txt.mms.net 
+#2409797031@mms.att.net
+crontab -l > mycron
+echo "05 08 * * * /home/taylor/git/gift-for-jazmine/send-message.sh" >> ./mycron
+crontab ./mycron
+rm ./mycron
