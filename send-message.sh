40ee5f4ddbd17959063c83b0a82c1237852d5a27
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-21 13:14:44 -0500

    added some finishing touches

--- send-message.sh
+++ send-message.sh
@@ -27,22 +27,21 @@ else
   fi
 fi
 
-#if [[ `date +%u` -eq 7 ]]; then
-#  echo "" > ${workdir}/randomdays
+if [[ `date +%u` -eq 7 ]]; then
+  echo "" > ${workdir}/randomdays
 
-#  days=()
+  days=()
 
-#  days+=($(( ( RANDOM % 7 ) + 1 )))
-#  days+=($(( ( RANDOM % 7 ) + 1 )))
-#  while [[ ${days[0]} -eq ${days[1]} ]]; do
-#    unset days[1]
-#    days+=($(( ( RANDOM % 7 ) + 1 )))
-#  done
-#  days+=($(( ( RANDOM % 7 ) + 1 )))
-#  while [[ ${days[2]} -eq ${days[1]} ]] || [[ ${days[2]} -eq ${days[0]} ]]; do
-#    unset days[2]
-#    days+=($(( ( RANDOM % 7 ) + 1 )))
-#  done
-#  echo ${days[@]} > ${workdir}/randomdays
-#fi
-
+  days+=($(( ( RANDOM % 7 ) + 1 )))
+  days+=($(( ( RANDOM % 7 ) + 1 )))
+  while [[ ${days[0]} -eq ${days[1]} ]]; do
+    unset days[1]
+    days+=($(( ( RANDOM % 7 ) + 1 )))
+  done
+  days+=($(( ( RANDOM % 7 ) + 1 )))
+  while [[ ${days[2]} -eq ${days[1]} ]] || [[ ${days[2]} -eq ${days[0]} ]]; do
+    unset days[2]
+    days+=($(( ( RANDOM % 7 ) + 1 )))
+  done
+  echo ${days[@]} > ${workdir}/randomdays
+fi
