5d98a3182d7ba684b45af17de6e47ce592a543cb
bluesnogbox <mybluesnogbox@gmail.com>
2014-12-23 21:17:39 -0500

    updated .gitignore

--- project-init.sh
+++ project-init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~
+echo "The message should have been sent to Jazmine" | mail -s "project-init" 8149773797@mms.att.net
 echo "I hope you like the gift I've been working on for you. You can read about it at https://github.com/bluesnogbox/gift-for-jazmine" | mail -s "Hey You!" jehackett0@gmail.com 
 crontab -l > mycron
 echo "05 08 * * * /home/taylor/git/gift-for-jazmine/send-message.sh" >> ./mycron
